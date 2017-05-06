b5c70c79616091a72de22a97e79df25aaffafeac
Pasquale Rocchio <patrocc6@gmail.com>
2017-05-06 11:41:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute_group, attribute_data|
+    attribute_data.each do |attribute, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon] ||= {}
+        pigeon_list[pigeon][attribute_group] ||= []
+        pigeon_list[pigeon][attribute_group] << attribute.to_s
+      end
+    end
+  end
+  pigeon_list
 end
